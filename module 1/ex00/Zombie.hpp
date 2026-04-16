fbb788f5b8f2ab3895f53d55320163943197f8e0
SirMengo <msimoes0103@gmail.com>
2026-04-16 18:30:01 +0100

    Started CPP1

---
+++ module 1/ex00/Zombie.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/16 03:57:02 by msimoes           #+#    #+#             */
+/*   Updated: 2026/04/16 04:11:23 by msimoes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+
+class Zombie
+{
+	private:
+		std::string name;
+	public:
+		void announce();
+		Zombie* newZombie( std::string name );
+		void randomChump( std::string name );
+}
+
+#endif
