5e596695b15b643633dc036a1c85b2b2dd580a45
SirMengo <msimoes0103@gmail.com>
2026-05-13 15:18:02 +0100

    Almost finished

--- module 1/ex05/Harl.hpp
+++ module 1/ex05/Harl.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Harl.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mago <mago@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: msimoes@student.42lisboa.com <msimoes>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 11:32:53 by mago              #+#    #+#             */
-/*   Updated: 2026/05/07 11:39:13 by mago             ###   ########.fr       */
+/*   Updated: 2026/05/13 15:17:41 by msimoes@stu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@
 class Harl
 {
     public:
+    	Harl();
+		~Harl();
         void complain(std::string level);
+        void	(Harl::*actions[4])(void);
     private:
         void debug(void);
         void info(void);
--- module 1/ex05/Harlp.cpp
+++ module 1/ex05/Harlp.cpp
@@ -3,12 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   Harlp.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mago <mago@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: msimoes@student.42lisboa.com <msimoes>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 11:32:40 by mago              #+#    #+#             */
-/*   Updated: 2026/05/07 11:32:50 by mago             ###   ########.fr       */
+/*   Updated: 2026/05/13 14:56:33 by msimoes@stu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
+Harl::Harl()
+{
+    std::cout << "Harl is ready to complain!" << std::endl;
+}
+
+Harl::~Harl()
+{
+    std::cout << "Harl has nothing more to complain about." << std::endl;
+}
+
+void Harl::debug()
+{
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!";
+}
+
+void Harl::info()
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!";
+}
+
+void Harl::warning()
+{
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.";
+}
+
+void Harl::error()
+{
+    std::cout << "This is unacceptable! I want to speak to the manager now.";
+}
+
+void Harl::complain(std::string level)
+{
+	std::string tags[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*actions[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	for(int i = 0; i < 4; i++)
+	{
+		if (level == tags[i])
+		{
+			(this->*actions[i])();
+			break;
+		}
+	}
+}
